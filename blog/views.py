960bceaa09b3566e2c04df4c5377b2f557770d32
MonssifElkadi <elkadi.monssif@etu.uae.ac.ma>
2026-03-26 19:01:39 +0100

    add of authentification syster

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import get_object_or_404, render , redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     posts = Post.objects.order_by('-created_at')
     return render(request , 'blog/index.html' , {'posts' : posts})
 
+@login_required
 def detail(request , id):
     post = get_object_or_404(Post , pk=id)
     return render(request , 'blog/details.html' , {'post' : post})
 
+@login_required
 def create(request):
     form = PostForm(request.POST or None)
     if (form.is_valid()):
@@ -19,6 +23,7 @@ def create(request):
         form = PostForm()
     return render(request, 'blog/create.html', {'form' : form})
 
+@login_required
 def modify(request , id):
     post = get_object_or_404(Post , pk=id)
     if request.method == "POST":
@@ -29,7 +34,8 @@ def modify(request , id):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/edit.html', {'form': form, 'post': post})
-        
+
+@login_required     
 def delete(request, id):
     post = get_object_or_404(Post, pk=id)
 
